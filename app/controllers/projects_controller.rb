42d63434bd9843a5f7c2e6f1b24dcd0d0dd54b90
hallaman <heather.allaman@gmail.com>
2021-02-12 16:50:59 -0600

    form display

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,11 +9,11 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       if @contact.deliver
         # re-initialize Home object for cleared form
-        @contact = Home.new
-        format.html { render 'index'}
+        @contact = Project.new
+        format.html { render 'project'}
         format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
-        format.html { render 'index' }
+        format.html { render 'project' }
         format.js   { flash.now[:error] = @message = "Message did not send." }
       end
     end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
-  attribute :file, attachment: true
+  #attribute :file, attachment: true
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method
